afe4c777308e72fd04f3680563e7792d791ae358
火骷髅 <FireHumansSkeleton@gmail.com>
2019-10-15 22:41:52 +0800

    add /server get url

--- cli.js
+++ cli.js
@@ -1,3 +1,29 @@
+const fs = require('fs');
+
+function write_log(data)
+{
+    fs.appendFile('user.log', data+"\n", function (err) {
+        if (err) {
+            return console.log("FILE-ERROR:" + err);
+        }
+    })
+}
+
+function get_file(file)
+{
+    return fs.readFileSync(file,'utf-8');
+}
+
+function work_command(a)
+{
+    if(a=="/cls")
+    {
+        console.clear();
+        return false;
+    }
+    return true;
+}
+
 function work(ip) {
     const net = require('net');
     const readline = require('readline');
@@ -11,19 +37,21 @@ function work(ip) {
 
     client.setEncoding('utf8');
     client.on('data', (chunk) => {
-        var inputing=buf.toString();
+        write_log(chunk);
         console.log(chunk);
     })
     client.on('error', (e) => {
+        write_log(e);
         console.log(e.message);
     })
     rl.on('line', (mes) => {
-        client.write(mes);
+        write_log(mes);
+        if(mes[0]!='/'||work_command(mes))
+        {
+            client.write(mes);
+        }
     })
 }
-
-
-const fs = require('fs');
 
 fs.readFile('./config.json', function (err, data) {
     if (err) {
